05bc9a8f01121e1d612fc1575fee4e6b1c11fe03
Max-Ryujin <61623298+Max-Ryujin@users.noreply.github.com>
2020-05-18 13:37:00 +0200

    2.7.0 b

--- src/main/java/commends/cmdall.java
+++ src/main/java/commends/cmdall.java
@@ -1,7 +1,7 @@
 package commends;
 
 import core.Leveldb;
-import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class cmdall implements Command{
@@ -15,16 +15,22 @@ public class cmdall implements Command{
         if(event.getAuthor().getIdLong() == 188893043577847808l||event.getAuthor().getIdLong() == 260455062093037568l) {
             for (int i = 0; i < Leveldb.members.size(); i++) {
                 long userid = Leveldb.members.get(i)[0];
-                Member user;
+                User user;
                 try {
-                    user = event.getGuild().retrieveMemberById(userid).complete();
+                    user = event.getJDA().retrieveUserById(userid).complete();
                 }
                 catch(Exception e)
                 {
                     user = null;
                 }
                 if(user != null) {
-                    event.getChannel().sendMessage(i + 1 + ". " + user.getEffectiveName()).queue();
+                    if(event.getGuild().getMember(user) != null) {
+                        event.getChannel().sendMessage(i + 1 + ". Level: " + Leveldb.members.get(i)[2] + " | " + user.getName() + " | " + event.getGuild().getMember(user).getEffectiveName()).queue();
+                    }
+                    else
+                    {
+                        event.getChannel().sendMessage(i + 1 + ". Level: " + Leveldb.members.get(i)[2] + " | " + user.getName()).queue();
+                    }
                 }
                 else
                 {
